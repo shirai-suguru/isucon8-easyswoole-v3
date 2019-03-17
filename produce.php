355475ee349c0a205703349530392496d8244440
Suguru Shirai <shirai_suguru@cyberagent.co.jp>
2019-03-17 18:24:48 +0900

    Modify MySQL Timeout second to 60s

--- produce.php
+++ produce.php
@@ -43,7 +43,7 @@ return [
         'host'          => '127.0.0.1',
         'port'          => '3306',
         'user'          => 'isucon',
-        'timeout'       => '5',
+        'timeout'       => '60',
         'charset'       => 'utf8mb4',
         'password'      => 'isucon',
         'database'      => 'torb',
